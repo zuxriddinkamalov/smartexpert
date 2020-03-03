446d442de0881b2968d84888f5d67f577fda6285
Zuxriddin Kamalov <bigjoss@mail.ru>
2020-03-03 20:11:51 +0500

    Add top navigation

--- pages/Community.vue
+++ pages/Community.vue
@@ -1,6 +1,15 @@
 <template>
   <Container>
-    <groups />
+    <div class="community">
+      <el-radio-group v-model="tab" class="community__active-page">
+        <el-radio-button label="post_lent">Лента постов</el-radio-button>
+        <el-radio-button label="my_groups">Мои группы</el-radio-button>
+        <el-radio-button label="help_requests"
+          >Запросы о помощи</el-radio-button
+        >
+      </el-radio-group>
+      <groups />
+    </div>
   </Container>
 </template>
 
@@ -10,8 +19,29 @@ import Container from '~/components/Container'
 export default {
   name: 'Community',
   components: { Groups, Container },
-  layout: 'admin'
+  layout: 'admin',
+  data() {
+    return {
+      tab: 'my_groups'
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.community {
+  max-width: 931px;
+  &__active-page {
+    margin-bottom: 15px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    & .el-radio-button {
+      flex-grow: 1;
+    }
+    & .el-radio-button__inner {
+      width: 100%;
+    }
+  }
+}
+</style>
